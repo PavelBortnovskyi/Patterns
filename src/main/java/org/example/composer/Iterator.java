ac552da4b868293f11fcdb2e54eba51b6909fdac
Pavel Bortnovskyi <bortnovskyi.pavlo@gmail.com>
2023-07-27 12:18:54 +0300

    composer pattern added refactor

--- src/main/java/org/example/composer/Iterator.java
+++
@@ -1,7 +0,0 @@
-package org.example.composer;
-
-//Этот итератор создан как промежуточный пример. Сам паттерн переведен на стоковый итератор Java
-public interface Iterator {
-  boolean hasNext();
-  MenuItem next();
-}
